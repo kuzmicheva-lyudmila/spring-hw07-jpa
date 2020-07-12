5501d8689627f2ff37f27aebf422769e28294c2a
ftc\kuzmicheva <l.kuzmicheva@cft.ru>
2020-07-12 19:34:34 +0700

    authentication

--- src/main/resources/db/migration/V2__AddUsers.sql
+++ src/main/resources/db/migration/V2__AddUsers.sql
@@ -5,6 +5,7 @@ create table if not exists library.users (
   active boolean not null default true
 );
 
+-- password: 1234
 insert into library.users(user_name, password)
 values ('user', '$2a$10$z4QtY3jNduuaWpMGw.EX0.EP5meB5oH0B1cCs0AGbzx6S7y5V00gK')
 on conflict do nothing;
